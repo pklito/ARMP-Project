0cd2d0b65ef14f7696afa3fbb3b9f4ad4bcfdaca
Saleh <salihhassan@campus.technion.ac.il>
2024-05-29 17:30:57 +0300

    bring in library

--- realsense_camera/stream_and_detect.py
+++ realsense_camera/stream_and_detect.py
@@ -3,6 +3,31 @@ import pyrealsense2 as rs
 import numpy as np
 from PIL import Image
 
+class CameraStreamer:
+    def __init__(self):
+        # Initialize RealSense camera pipeline
+        self.pipeline = rs.pipeline()
+        config = rs.config()
+        config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+        self.pipeline.start(config)
+    
+    def get_frames(self):
+        frames = self.pipeline.wait_for_frames()
+        depth_frame = frames.get_depth_frame()
+        color_frame = frames.get_color_frame()
+
+        if not depth_frame or not color_frame:
+            return None, None
+
+        depth_image = np.asanyarray(depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
+
+        return color_image, depth_image
+
+    def stop(self):
+        self.pipeline.stop()
+
 def calculate_similarity(color1, color2):
     return np.sum(np.abs(color1 - color2))
 

9bb581b396318cdf59a6030c36a294623ce3b4ef
Saleh <salihhassan@campus.technion.ac.il>
2024-05-29 17:11:02 +0300

    start moving ball

--- example_control_loop.py
+++ example_control_loop.py
@@ -32,6 +32,31 @@ import rtde.rtde_config as rtde_config
 from time import time
 from math import fmod
 
+import numpy as np
+from CameraController import *
+from Robots import *
+from realsense_camera import stream_and_detect
+
+
+
+def clamp(value, min, max):
+    if value < min:
+        return min
+    if value > max:
+        return max
+    return value
+
+# Config
+plate_center = (317, 356)
+wrist_3_balance = 269.47
+balanced_conf = [0.44, -2.22, -0.0, -0.93, 1.08, -1.57]
+
+
+
+# Settings
+max_error = 50
+kp = -0.08/60
+        
 # logging.basicConfig(level=logging.INFO)
 
 ROBOT_HOST = "192.168.0.11"
@@ -62,11 +87,6 @@ old_setp2 = [34.03, -115.56, -64.3, 0.2, 56.23, 269.47]
 # # Setpoints to move the robot to
 # setp1 = [0, -3.14 / 2, 0, -3.14 / 2, 0, 0]
 # setp2 = [(3.1415926*a)/180 for a in old_setp2]
-
-#setp2 = [-0.1,-1.94,0.04,-1.03,-0.43,0.09]
-setp2 = [0.56, -2.58, -0.0, -0.75, 0.85, 1.4]
-setp1 = [0.56, -2.58, -0.0, -0.75, 0.85, 0.0]
-
 
 setp.input_double_register_0 = 0
 setp.input_double_register_1 = 0
@@ -100,6 +120,28 @@ def full_print(state,move_completed = False):
 if not con.send_start():
     sys.exit()
 
+
+camera = CameraWrapper()
+
+def get_error():
+    ret, frame = camera.cap.read()
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        exit()
+        
+    position = stream_and_detect.detect_object(frame)[0]
+    if(position is None):
+        return None
+    
+    x1,y1,x2,y2 = position
+    ball_center = (int((x1+x2)/2), int((y1+y2)/2))
+    
+    error = plate_center[0] - ball_center[0]
+    print("error: ", plate_center[0] - ball_center[0])
+
+    return error
+    
+        
 # control loop
 move_completed = True
 while keep_running:
@@ -107,11 +149,17 @@ while keep_running:
     state = con.receive()
     if state is None:
         break
-    #full_print(state)
+    full_print(state)
     # do something...
     if move_completed and state.output_int_register_0 == 1:
         move_completed = False
-        new_setp = setp1 if fmod(time(),6) < 3 else setp2
+        
+        new_setp = balanced_conf
+        error = get_error()
+        if(error is None):
+            continue
+        new_setp[5] += kp * error
+        
         list_to_setp(setp, new_setp)
         print("New pose = " + str(new_setp))
         # send new setpoint

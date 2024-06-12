3b6bfafb17302add6de3681cf7885064fb437603
Paulo Khayat <paulokhayat@gmail.com>
2024-06-12 16:57:58 +0300

    start aruco image

--- realsense_camera/get_images.py
+++ realsense_camera/get_images.py
@@ -1,15 +1,35 @@
 from CameraStreamer import CameraStreamer
 import cv2
 import numpy as np
+arucoDict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_ARUCO_ORIGINAL)
+arucoParams = cv2.aruco.DetectorParameters()
+def get_aruco_corners(color_image):
 
-def try_chessboard_sizes(gray_image, sizes):
-    for size in sizes:
-        ret, corners = cv2.findChessboardCorners(gray_image, size, None)
-        if ret:
-            return ret, corners, size
-    return False, None, None
+    corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(color_image, arucoDict, parameters=arucoParams)
+    return ids, corners
+
+def get_object(aruco_ids):
+   # milimeters
+   SIZE = 52
+   TL_DISP = (-(9 + SIZE), 73 + SIZE,0)
+   TR_DISP = (8 + SIZE, 73 + SIZE,0)
+   BR_DISP = (6.5 + SIZE, -(58 + SIZE),0)
+   BL_DISP = (-(19 + SIZE),-( 64 + SIZE),0)
+   arucos_obj = dict()
+   base = [(-SIZE/2,SIZE/2,0),(SIZE/2,SIZE/2,0),(SIZE/2,-SIZE/2,0),(-SIZE/2,-SIZE/2,0)]
+   arucos_obj[200] = [[c/1000. for c in points] for points in base]
+   arucos_obj[300] = [[(coord + displacement)/1000. for coord, displacement in zip(points, TL_DISP)] for points in base]
+   arucos_obj[500] = [[(coord + displacement)/1000. for coord, displacement in zip(points, TR_DISP)] for points in base]
+   arucos_obj[400] = [[(coord + displacement)/1000. for coord, displacement in zip(points, BR_DISP)] for points in base]
+   arucos_obj[600] = [[(coord + displacement)/1000. for coord, displacement in zip(points, BL_DISP)] for points in base]
+
+   return [a for i in aruco_ids for a in arucos_obj[i]]
+
 
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+dist_coeff = np.load("dist.npy")
+matrix_coeff = np.load("mtx.npy")
 
 if __name__ == "__main__":
     count = 0
@@ -22,25 +42,19 @@ if __name__ == "__main__":
         color_image, depth_image, depth_frame, depth_colormap, depth_intrinsics = camera.get_frames()
         if color_image is None:
             continue
-        gray = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
+        ids, corners = get_aruco_corners(color_image)
+        if ids is not None:
+            object_pts =np.array([ np.array(a) for a in get_object([i[0] for i in ids.tolist()])], dtype=np.float64)
+            print(object_pts)
+            pixel_pts = corners
+            pixel_pts = np.array(pixel_pts, dtype=np.float64)
 
-        ret, corners, size = try_chessboard_sizes(gray, common_sizes)
-        if ret:
-            print(f"Detected chessboard size: {size}")
-            print(corners)
-            cv2.imwrite(f"realsense_camera/chessboard/{count}.png", color_image)
-            count += 1
-
-            corners2 = cv2.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
-            # Draw and display the corners
-            cv2.drawChessboardCorners(gray, (14,5), corners2, ret)
-            cv2.imshow('img', gray)
-        else:
-            cv2.imshow('img', gray)
-
+            # Ensure image_points has shape (N, 1, 2)
+            if pixel_pts.shape[1] == 2:
+                pixel_pts = pixel_pts.reshape(-1, 1, 2)
+            ret, rvec, tvec = cv2.solvePnP(object_pts,pixel_pts,matrix_coeff,dist_coeff)
+            print( rvec, tvec)
         key = cv2.waitKey(1) & 0xFF
-
- # If found, add object points, image points (after refining them)
         if key == ord('q'):
             cv2.destroyAllWindows()
             loop = False

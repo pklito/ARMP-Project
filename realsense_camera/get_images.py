6129b0178bd9f3fabb3bd5ccbf94a33b9f63b6d5
Paulo Khayat <paulokhayat@gmail.com>
2024-06-12 17:27:54 +0300

    draw rvec

--- realsense_camera/get_images.py
+++ realsense_camera/get_images.py
@@ -10,6 +10,8 @@ def get_aruco_corners(color_image):
     return ids, corners
 
 def get_object(aruco_ids):
+    if not all(a in [200,300,400,500,600] for a in aruco_ids):
+        return []
     # milimeters
     SIZE = 52
     TL_DISP = (-(9 + SIZE), 73 + SIZE, 0)
@@ -18,12 +20,12 @@ def get_object(aruco_ids):
     BL_DISP = (-(19 + SIZE), -(64 + SIZE), 0)
     arucos_obj = dict()
     base = [(-SIZE/2, SIZE/2, 0), (SIZE/2, SIZE/2, 0), (SIZE/2, -SIZE/2, 0), (-SIZE/2, -SIZE/2, 0)]
-    arucos_obj[200] = [[c/1000. for c in points] for points in base]
+    arucos_obj[200] = [[c for c in points] for points in base]
     arucos_obj[300] = [[(coord + displacement)/1000. for coord, displacement in zip(points, TL_DISP)] for points in base]
     arucos_obj[500] = [[(coord + displacement)/1000. for coord, displacement in zip(points, TR_DISP)] for points in base]
     arucos_obj[400] = [[(coord + displacement)/1000. for coord, displacement in zip(points, BR_DISP)] for points in base]
     arucos_obj[600] = [[(coord + displacement)/1000. for coord, displacement in zip(points, BL_DISP)] for points in base]
-
+    
     return [a for i in aruco_ids for a in arucos_obj[i]]
 
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
@@ -46,24 +48,26 @@ if __name__ == "__main__":
         if ids is not None:
             object_pts = np.array([a for i in ids for a in get_object([i[0]])], dtype=np.float32)
             pixel_pts = np.array([c for aruco in corners for c in aruco[0]], dtype=np.float32)
+            if(len(object_pts)!=len(pixel_pts)):
+                print("Error, sizes", len(object_pts)!=len(pixel_pts))
+            else:
+                if pixel_pts.ndim == 3 and pixel_pts.shape[1] == 1 and pixel_pts.shape[2] == 4:
+                    pixel_pts = pixel_pts[:, 0, :]
 
-            if pixel_pts.ndim == 3 and pixel_pts.shape[1] == 1 and pixel_pts.shape[2] == 4:
-                pixel_pts = pixel_pts[:, 0, :]
+                if object_pts.size == 0 or pixel_pts.size == 0:
+                    continue
 
-            if object_pts.size == 0 or pixel_pts.size == 0:
-                continue
+                ret, rvec, tvec = cv2.solvePnP(object_pts, pixel_pts, matrix_coeff, dist_coeff)
 
-            ret, rvec, tvec = cv2.solvePnP(object_pts, pixel_pts, matrix_coeff, dist_coeff,flags = cv2.SOLVEPNP_SQPNP)
+                rotation_matrix, _ = cv2.Rodrigues(rvec)
 
-            rotation_matrix, _ = cv2.Rodrigues(rvec)
+                # Define homogeneous coordinates for the pixel (x, y, 0)
+                homogeneous_coords = np.array([320,240, 0, 1])
 
-            # Define homogeneous coordinates for the pixel (x, y, 0)
-            homogeneous_coords = np.array([320,240, 0, 1])
+                extrinsic_matrix = np.hstack((rotation_matrix, tvec))
+                cv2.drawFrameAxes(color_image, matrix_coeff, dist_coeff, rvec, tvec, 10, 2)
 
-            extrinsic_matrix = np.hstack((rotation_matrix, tvec))
-            print(np.dot(extrinsic_matrix,homogeneous_coords))
-
-
+        cv2.imshow("i,", color_image)
         key = cv2.waitKey(1) & 0xFF
         if key == ord('q'):
             cv2.destroyAllWindows()

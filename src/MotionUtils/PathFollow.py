1787ddcaa40372123bd62fd1578f86b05de4ff37
Paulo Khayat <paulokhayat@gmail.com>
2024-06-23 18:54:37 +0300

    Reintroduce PathFollow from robotplanning branch

--- src/MotionUtils/PathFollow.py
+++ src/MotionUtils/PathFollow.py
@@ -1,6 +1,9 @@
 import numpy as np
 from math import fmod, modf
-from motionConstants.constants import *
+from .motionConstants.constants import *
+# This python code is meant to follow a path.
+# Input: a list of (x,y) coordinates.
+# Stream: updates of robot position or returns NaN if not updated.
 
 def clamp(val, v_min, v_max):
     return min(v_max, max(v_min,val))
@@ -40,18 +43,30 @@ def getClampedLookahead(point, target, lookahead_distance):
         return target
 
     return point + lookahead_distance * target_vector / target_distance
+
+def getPointFromT(path ,edge_num, t):
+        if edge_num >= len(path) - 1:
+            return np.array(path[-1])
+        p1 = np.asarray(path[edge_num])
+        p2 = np.asarray(path[edge_num + 1])
+
+        return (1-t) * p1 + t * p2
+
+
 class PathFollowStrict:
     path = None     #List of the path configurations
     path_edges = None       #List of edges I.E. pairs of points
     PATH_LOOKAHEAD = 0.3
     EDGE_CUTOFF = 0.3
     current_edge = 0
+    _ret_target_t = 0
     def __init__(self, path, path_lookahead = TASK_PATH_LOOKAHEAD, EDGE_CUTOFF = TASK_EDGE_CUTOFF):
         self.path = path
         self.path_edges = [edge for edge in zip(self.path, self.path[1:])]
         self.PATH_LOOKAHEAD = path_lookahead
         self.EDGE_CUTOFF = EDGE_CUTOFF
 
+    # doesn't change self
     def getLookaheadConfig(self, config, lookahead_distance = None):
         if lookahead_distance == None:
             lookahead_distance = self.PATH_LOOKAHEAD
@@ -66,13 +81,23 @@ class PathFollowStrict:
         edge_length = np.linalg.norm(edge[1] - np.array(edge[0]))
 
         target = None
+        target_t = 0
         if edge_length <= 0.001:
             target = edge[1]
+            target_t = 1
         else:
-            target = self.getPointFromT(self.current_edge, clamp(t + lookahead_distance / edge_length, 0, 1))
+            target_t = clamp(t + lookahead_distance / edge_length, 0, 1)
+            target = getPointFromT(self.path, self.current_edge, target_t)
 
+        self._ret_target_t = target_t # For returning
         return getClampedLookahead(point, target, lookahead_distance)
 
+    # Maintains previous functionality of getLookaheadConfig albiet more ugly implementation.
+    def getLookaheadAndT(self, config, lookahead_distance = None):
+        lookahead = self.getLookaheadConfig(config, lookahead_distance)
+        return lookahead, self.current_edge, self._ret_target_t
+
+    #Changes self
     def updateCurrentEdge(self, config, cutoff_radius = None):
         if cutoff_radius == None:
             cutoff_radius = self.EDGE_CUTOFF
@@ -82,11 +107,4 @@ class PathFollowStrict:
         if np.linalg.norm(np.asarray(config) - self.path[self.current_edge + 1]) < cutoff_radius:
             self.current_edge += 1
 
-    def getPointFromT(self,edge_num, t):
-        if edge_num >= len(self.path_edges):
-            return np.array(self.path_edges[-1][1])
-        p1 = np.asarray(self.path_edges[edge_num][0])
-        p2 = np.asarray(self.path_edges[edge_num][1])
-
-        return (1-t) * p1 + t * p2
 

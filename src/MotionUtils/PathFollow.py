74516bfa70713e00654f6850f5ce840ea16547a5
Paulo Khayat <paulokhayat@gmail.com>
2024-06-23 20:46:16 +0300

    add edge case for lookahead = 0

--- src/MotionUtils/PathFollow.py
+++ src/MotionUtils/PathFollow.py
@@ -37,6 +37,8 @@ def getEdgeProjection(config, edge):
     return projection, clamp(t, 0, 1), clamp(t_distance, 0, edge_length)
 
 def getClampedTarget(point, target, lookahead_distance):
+    if lookahead_distance < 0.00001:
+        return point
     target_vector = np.asarray(target) - point
     target_distance = np.linalg.norm(target_vector)
     if target_distance <= lookahead_distance:

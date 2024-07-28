9e0a8c5987d7543ada2ea16da826da4afa2a81e4
Paulo Khayat <paulokhayat@gmail.com>
2024-07-28 15:24:32 +0300

    Color in arucos

--- ErrorDrawer.py
+++ ErrorDrawer.py
@@ -21,6 +21,11 @@ from sklearn.preprocessing import normalize
 file_path = 'Square_path_log.txt'
 log_data = None
 
+# 'True': change color. 'False': drawn statically. 'None': don't draw.
+DO_ARUCOS_COLOR = True
+# True: draw robot wrist deadzone, False dont.
+DO_ROBOT_ARM = True
+
 with open(file_path, 'r') as file:
     log_data = file.read()
 
@@ -34,10 +39,13 @@ matches = pattern.findall(log_data)
 
 timestamps = []
 errors = []
-
+arucos_list = []
 for match in matches:
     arucos, timestamp, error, robot_pos_str, target_pos_str = match
     # print("match: ", match)
+    arucos_cleaned = arucos.replace('[','').replace(']','')
+    arucos_values = [int(x.strip()) for x in arucos_cleaned.split('\n')]
+    arucos_list.append(arucos_values)
     timestamps.append(float(timestamp))
     if error != "None":
         error_cleaned = error.replace('array([', '').replace('])', '').replace(' ', '')
@@ -62,9 +70,9 @@ PLATE_DOWN = -plate_height/2
 PLATE_CENTER = (np.average([PLATE_LEFT,PLATE_RIGHT]), np.average([PLATE_DOWN,PLATE_UP]))
 
 # flip axis: x = y, y = -x
-filtered_errors = [(t, y, -x) for t, x, y, z in zip(normalized_timestamps, error_x, error_y, error_z) if z == 0.035]
+filtered_errors = [(t, y, -x, a) for t, x, y, z, a in zip(normalized_timestamps, error_x, error_y, error_z, arucos_list) if z == 0.035]
 
-normalized_timestamps, ball_positions_x, ball_positions_y = zip(*filtered_errors)
+normalized_timestamps, ball_positions_x, ball_positions_y, arucos = zip(*filtered_errors)
 
 FRAMERATE = 30
 print("Length of timestamps:", len(normalized_timestamps))
@@ -76,20 +84,20 @@ print("total time of simulation:", normalized_timestamps[-1] - normalized_timest
 timed_errors = []
 lt = normalized_timestamps[0]-1/FRAMERATE
 taccum = 0
-for t, x, y in filtered_errors:
+for t, x, y, a in filtered_errors:
     if taccum < -1/FRAMERATE:           # Only add a value if time difference is significant enough.
         taccum += 1/FRAMERATE
     elif taccum > 1/FRAMERATE:
-        timed_errors.append((t,x,y))
-        timed_errors.append((t,x,y))
+        timed_errors.append((t,x,y,a))
+        timed_errors.append((t,x,y,a))
         taccum += t - lt - 2/FRAMERATE
         lt = t
     else:
-        timed_errors.append((t,x,y))
+        timed_errors.append((t,x,y,a))
         taccum += t - lt - 1/FRAMERATE
         lt = t
 
-normalized_timestamps, ball_positions_x, ball_positions_y = zip(*timed_errors)
+normalized_timestamps, ball_positions_x, ball_positions_y, arucos = zip(*timed_errors)
 print("Length of timeset timestamps:", len(normalized_timestamps))
 print("total time of simulation:", normalized_timestamps[-1] - normalized_timestamps[0], "simulated time: ", len(normalized_timestamps)/FRAMERATE)
 
@@ -106,10 +114,11 @@ plt.grid(True, 'major')
 
 
 ## BALL MARKER SIZE V V V
-ball_screen_radius = 0.013
+ball_screen_radius = 0.035
 # # Add robot hand square
-square = patches.Rectangle((PLATE_CENTER[0]-0.035+ball_screen_radius/2,PLATE_CENTER[1]+0.033+ball_screen_radius), 0.07-ball_screen_radius, 0.1, linewidth=1, alpha=0.2, facecolor='b',hatch='//')
-ax.add_patch(square)
+if DO_ROBOT_ARM:
+    square = patches.Rectangle((PLATE_CENTER[0]-0.035+ball_screen_radius/2,PLATE_CENTER[1]+0.033+ball_screen_radius), 0.07-ball_screen_radius, 0.1, linewidth=1, alpha=0.2, facecolor='b',hatch='//')
+    ax.add_patch(square)
 
 # # add arucos
 VALID_ARUCOS = [a for a in range(12)]
@@ -118,7 +127,8 @@ VERT_DISP = 23 + SIZE
 HOR_DISP = 23 + SIZE
 SQUARE_SHAPE = [(-SIZE/2, SIZE/2), (SIZE/2, SIZE/2), (SIZE/2, -SIZE/2), (-SIZE/2, -SIZE/2)]
 SHAPE = (3,4)
-ARUCO_OBJ = [[((HOR_DISP * (i) + d[0] - HOR_DISP*(SHAPE[0]-1)/2) / 1000., (VERT_DISP*(-j) + d[1] + VERT_DISP*(SHAPE[1]-1)/2)/1000., 0) for d in SQUARE_SHAPE]  for j in range(SHAPE[1]) for i in range(SHAPE[0])]
+ARUCO_OBJ = [[(-1*(HOR_DISP * (i) + d[0] - HOR_DISP*(SHAPE[0]-1)/2) / 1000., (VERT_DISP*(-j) + d[1] + VERT_DISP*(SHAPE[1]-1)/2)/1000., 0) for d in SQUARE_SHAPE]  for j in range(SHAPE[1]) for i in range(SHAPE[0])]
+
 for aruco in ARUCO_OBJ:
     square = patches.Rectangle((PLATE_CENTER[0]+aruco[3][1],PLATE_CENTER[1]+aruco[3][0]), SIZE/1000., SIZE/1000., alpha=0.15, facecolor='black')
     ax.add_patch(square)
@@ -129,16 +139,29 @@ old_dots, = plt.plot([],[], 'x', markersize=8)
 
 def init():
     ball_dot.set_data([], [])
+    if DO_ARUCOS_COLOR is True:
+        return old_dots, ball_dot
     return old_dots, ball_dot
 
 def update(frame):
-    if frame < len(ball_positions_x):
-        ball_dot.set_data([ball_positions_x[frame]], [ball_positions_y[frame]])
-        old_dots.set_data(ball_positions_x[max(frame-10,0):max(frame-1,0)], ball_positions_y[max(frame-10,0):max(frame-1,0)])
+    ball_dot.set_data([ball_positions_x[frame]], [ball_positions_y[frame]])
+    old_dots.set_data(ball_positions_x[max(frame-10,0):max(frame-1,0)], ball_positions_y[max(frame-10,0):max(frame-1,0)])
+    if DO_ARUCOS_COLOR is True:
+        patch_list = []
+        for index, aruco in enumerate(ARUCO_OBJ):
+            patch_list.append(patches.Rectangle((PLATE_CENTER[0]+aruco[2][1],PLATE_CENTER[1]+aruco[2][0]), SIZE/1000., SIZE/1000., alpha=0.15, facecolor=('green' if index in arucos[frame] else 'black')))
+        for p in ax.patches:
+            p.remove()
+        for p in patch_list:
+            ax.add_patch(p)
+        if DO_ROBOT_ARM:
+            square = patches.Rectangle((PLATE_CENTER[0]-0.035+ball_screen_radius/2,PLATE_CENTER[1]+0.033+ball_screen_radius), 0.07-ball_screen_radius, 0.1, linewidth=1, alpha=0.2, facecolor='b',hatch='//')
+            ax.add_patch(square)
+        return old_dots, ball_dot
     return old_dots, ball_dot
 
 try:
     anim = FuncAnimation(plt.gcf(), update, frames=len(normalized_timestamps), init_func=init, blit=True)
-    anim.save('ball_simulation6.gif', writer='pillow', fps=30)
+    anim.save('ball_simulation8.gif', writer='pillow', fps=30)
 except Exception as e:
     print(f"Error saving GIF: {e}")

a93f991c007eb4a7aa45e198a2cc4a5195ba2cb6
Saleh <salihhassan@campus.technion.ac.il>
2024-07-28 08:44:51 +0300

    save changes

--- ErrorDrawer.py
+++ ErrorDrawer.py
@@ -2,18 +2,16 @@ import matplotlib.pyplot as plt
 import re
 import numpy as np
 from matplotlib.animation import FuncAnimation
-from PIL import Image
-from matplotlib.animation import PillowWriter
-from regex import F
 
-# def init():
-#     ball_dot.set_data([], [])
-#     return ball_dot,
+def init():
+    ball_dot.set_data([], [])
+    return old_dots, ball_dot
 
-# def update(frame):
-#     if frame < len(ball_positions_x):
-#         ball_dot.set_data(ball_positions_x[frame], ball_positions_y[frame])
-#     return ball_dot,
+def update(frame):
+    if frame < len(ball_positions_x):
+        ball_dot.set_data([ball_positions_x[frame]], [ball_positions_y[frame]])
+        old_dots.set_data(ball_positions_x[max(frame-10,0):max(frame-1,0)], ball_positions_y[max(frame-10,0):max(frame-1,0)])
+    return old_dots, ball_dot
 
 file_path = 'Square_path_log.txt'
 log_data = None
@@ -34,15 +32,13 @@ errors = []
 
 for match in matches:
     timestamp, error, robot_pos_str, target_pos_str = match
-    # print("match: ", match)
     timestamps.append(float(timestamp))
     if error != "None":
         error_cleaned = error.replace('array([', '').replace('])', '').replace(' ', '')
         error_values = np.array([float(x) for x in error_cleaned.split(',')])
         errors.append(error_values)
-        # print("error_values: ", error_values)
     else:
-        errors.append([0.0, 0.0, 0.0])  # Assuming zero error if not specified
+        errors.append([0.0, 0.0, 0.0])
 
 
 start_time = timestamps[0]
@@ -57,17 +53,17 @@ plate_width, plate_height = 0.21, 0.297
 ball_positions_x = np.clip(np.array([-x for x, _ in filtered_errors]) + plate_width / 2, 0, plate_width) # pay attention to '- sign', added for mirroring
 ball_positions_y = np.clip(np.array([-y for _, y in filtered_errors]) + plate_height / 2, 0, plate_height) # pay attention to '- sign', added for mirroring
 
-print("Length of timestamps:", len(timestamps))
-print("Length of ball_positions_x:", len(ball_positions_x), ball_positions_x)
-print("Length of ball_positions_y:", len(ball_positions_y), ball_positions_y)
+# print("Length of timestamps:", len(timestamps))
+# print("Length of ball_positions_x:", len(ball_positions_x), ball_positions_x)
+# print("Length of ball_positions_y:", len(ball_positions_y), ball_positions_y)
 
-print("Printing X positions: ")
-for x in ball_positions_x:
-    print(x)
+# print("Printing X positions: ")
+# for x in ball_positions_x:
+#     print(x)
 
-print("Printing Y positions: ")
-for y in ball_positions_y:
-    print(y)
+# print("Printing Y positions: ")
+# for y in ball_positions_y:
+#     print(y)
 
 fig, ax = plt.subplots()
 ax.set_xlim(0, plate_width)
@@ -77,18 +73,9 @@ old_dots, = plt.plot([],[], 'x', markersize=13)
 
 plt.grid(True)
 
-def init():
-    ball_dot.set_data([], [])
-    return old_dots, ball_dot
-
-def update(frame):
-    if frame < len(ball_positions_x):
-        ball_dot.set_data([ball_positions_x[frame]], [ball_positions_y[frame]])
-        old_dots.set_data(ball_positions_x[max(frame-10,0):max(frame-1,0)], ball_positions_y[max(frame-10,0):max(frame-1,0)])
-    return old_dots, ball_dot
 
 try:
     anim = FuncAnimation(fig, update, frames=len(timestamps), init_func=init, blit=True)
-    anim.save('ball_simulation4.gif', writer='pillow', fps=30)
+    anim.save('ball_simulation5.gif', writer='pillow', fps=30)
 except Exception as e:
     print(f"Error saving GIF: {e}")
